6f3ac06bbfc124f7223a416579e55e8e59fdce5d
Oscar Rodriguez <rodriguezmiraoscar@gmail.com>
2021-07-01 17:59:02 +0200

    docs agregado

---
+++ docs/precache-manifest.7865a64843c88394b6d80a169d08893a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1678bc4ba8d7c399522bc2449f9ee69d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "874c28e496d8130df6cd",
+    "url": "/static/css/main.43cf1c0e.chunk.css"
+  },
+  {
+    "revision": "7a20b51d34961c30974c",
+    "url": "/static/js/2.6606d5d4.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.6606d5d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874c28e496d8130df6cd",
+    "url": "/static/js/main.4705475f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
